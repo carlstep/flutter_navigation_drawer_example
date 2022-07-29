e4a47370a90f43800f4bf79c56a98567b785d8c1
carlstep <carlstep333@me.com>
2022-07-29 10:57:52 +0700

    setting up ListTile within ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_navigation_drawer_example/defaults/defaults.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -57,21 +59,143 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Navigation Example',
         ),
       ),
       body: pages[indexClicked],
       drawer: Drawer(
-        child: ListView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             DrawerHeader(
-              child: Image(
-                image: AssetImage(
-                  'assets/images/drawer.jpg',
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    'assets/images/drawer.jpg',
+                  ),
                 ),
               ),
-            )
+              padding: EdgeInsets.zero,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(
+                        'assets/images/profile.jpg',
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'John Rambo',
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            color: Colors.grey[300],
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          'john@rambo.com',
+                          style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            color: Colors.grey[300],
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    onTap: () {
+                      setState(() {
+                        indexClicked = 0;
+                      });
+                      Navigator.pop(context);
+                    },
+                    leading: Icon(
+                      Defaults.drawerItemIcon[0],
+                      size: 30,
+                      color: indexClicked == 0
+                          ? Defaults.drawerItemSelectedColor
+                          : Defaults.drawerItemColor,
+                    ),
+                    title: Text(
+                      Defaults.drawerItemText[0],
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: indexClicked == 0
+                            ? Defaults.drawerItemSelectedColor
+                            : Defaults.drawerItemColor,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      setState(() {
+                        indexClicked = 1;
+                      });
+                      Navigator.pop(context);
+                    },
+                    leading: Icon(
+                      Defaults.drawerItemIcon[1],
+                      size: 30,
+                      color: indexClicked == 1
+                          ? Defaults.drawerItemSelectedColor
+                          : Defaults.drawerItemColor,
+                    ),
+                    title: Text(
+                      Defaults.drawerItemText[1],
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: indexClicked == 1
+                            ? Defaults.drawerItemSelectedColor
+                            : Defaults.drawerItemColor,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      setState(() {
+                        indexClicked = 2;
+                      });
+                      Navigator.pop(context);
+                    },
+                    leading: Icon(
+                      Defaults.drawerItemIcon[2],
+                      size: 30,
+                      color: indexClicked == 2
+                          ? Defaults.drawerItemSelectedColor
+                          : Defaults.drawerItemColor,
+                    ),
+                    title: Text(
+                      Defaults.drawerItemText[2],
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: indexClicked == 2
+                            ? Defaults.drawerItemSelectedColor
+                            : Defaults.drawerItemColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
