f0f341492ceb8d1ccd2faf2d1ab21210a7c29f91
carlstep <carlstep333@me.com>
2022-07-29 10:08:04 +0700

    initialize the drawer with ListView

--- lib/defaults/defaults.dart
+++ lib/defaults/defaults.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 
 class Defaults {
   static const Color drawerItemColor = Colors.blueGrey;
-  static const Color drawerItemSelectedColor =
-      Color.fromARGB(255, 25, 118, 210);
-  static const Color drawerSelectedTileColor = Color.fromARGB(94, 89, 155, 212);
+  static const Color drawerItemSelectedColor = Colors.lightBlue;
+  //static const Color drawerSelectedTileColor = Color.fromARGB(94, 89, 155, 212);
 
   static final drawerItemText = [
     'Inbox',
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,80 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+var indexClicked = 0;
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final pages = [
+    Center(
+      child: Text(
+        'Inbox',
+      ),
+    ),
+    Center(
+      child: Text(
+        'Starred',
+      ),
+    ),
+    Center(
+      child: Text(
+        'Sent',
+      ),
+    ),
+    Center(
+      child: Text(
+        'Drafts',
+      ),
+    ),
+    Center(
+      child: Text(
+        'Trash',
+      ),
+    ),
+    Center(
+      child: Text(
+        'Spam',
+      ),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Navigation Example',
+        ),
+      ),
+      body: pages[indexClicked],
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Image(
+                image: AssetImage(
+                  'assets/images/drawer.jpg',
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
